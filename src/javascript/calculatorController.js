5d4f6bed9da8c99c4cf8a946a580f9f38904d658
Brandon Eastwell <be21160@essex.ac.uk>
2024-11-10 17:43:40 +0000

    calculator input buttons click events

--- src/javascript/calculatorController.js
+++ src/javascript/calculatorController.js
@@ -1,16 +1,47 @@
+//GLOBAL VAR
 let firstNum;
 let secondNum;
 let operator;
 
+//HTML DOM Elements
+const calcScreen = document.querySelector("#screen-text");
+let calcBtns = document.querySelector('#input-container');
+
+calcBtns.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.className.includes("btn")) {
+        calcScreen.textContent += target.textContent; //updates calculator screen
+        updateCalc();
+    }
+});
+
+const isEmpty = value => value === "";
+
+function updateCalc() {
+    //if the first number variable is empty find the first value
+    //ignore operators
+    //once found the first value find the first operator between the values
+    //find the second number if the second number variable is empty
+    //once all variables contain values call operate function and parse all values
+    //if CLEAR input is called empty all values and current text content of screen
+    const arrInput = calcScreen.textContent.split("");
+    arrInput.forEach((value) => {
+        if (isEmpty(firstNum)) {
+
+        }
+    })
+}
+
 function operate(firstNum, secondNum, operator) {
-    if (operator === '+') {
-        return add(firstNum, secondNum);
-    } else if (operator === '-') {
-        return subtract(firstNum, secondNum);
-    } else if (operator === 'x') {
-        return multiply(firstNum, secondNum);
-    } else if (operator === '/') {
-        return divide(firstNum, secondNum);
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum, secondNum);
+        case 'x':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
     }
 }
 
