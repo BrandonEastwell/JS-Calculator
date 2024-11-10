f8e8f549ff191a10df457cf941a716e9324e31e5
Brandon Eastwell <be21160@essex.ac.uk>
2024-11-10 15:40:12 +0000

    input controls, layout and styling

---
+++ src/javascript/calculatorController.js
@@ -0,0 +1,28 @@
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(firstNum, secondNum, operator) {
+    if (operator === '+') {
+        return add(firstNum, secondNum);
+    } else if (operator === '-') {
+        return subtract(firstNum, secondNum);
+    } else if (operator === 'x') {
+        return multiply(firstNum, secondNum);
+    } else if (operator === '/') {
+        return divide(firstNum, secondNum);
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
